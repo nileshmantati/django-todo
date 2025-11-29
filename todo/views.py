149a8dbaa36c73f76e92128a03f73392cb1a14ba
Sanjay Mantati <sanjayms9510@gmail.com>
2025-11-29 10:52:09 +0530

    add  functionality like delete and edit

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404, render
 from .models import Task
+
 # Create your views here.
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task = task)
     return redirect('home')
+
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
+def delete_task(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
+def edit_task(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    if request.method == 'POST':
+        task.task = request.POST['task']
+        task.save()
+        return redirect('home')
+        
+    return render(request,'home.html',{
+        'edit_task':task,
+        'not_completed_tasks': Task.objects.filter(is_completed=False).order_by('-updated_at'),
+        'completed_tasks': Task.objects.filter(is_completed=True).order_by('-updated_at'),
+    })
--- todo_project/views.py
+++ todo_project/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from todo.models import Task
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    not_completed_tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
     context = {
-        'tasks': tasks,
+        'not_completed_tasks': not_completed_tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request,'home.html',context)
