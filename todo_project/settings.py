8d4931db37b67f5a06327411d551f4f096c667ac
Sanjay Mantati <sanjayms9510@gmail.com>
2025-11-29 11:22:27 +0530

    Prepare for Render deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6bm1=--y7gh$9ko66%bibn2m63b+qq_tlo_(kibx!ceqy#9gb1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
