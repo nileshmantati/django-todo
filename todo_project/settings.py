e25d1c0937bf72ca50909a1c3b208a1d433a1fb7
Sanjay Mantati <sanjayms9510@gmail.com>
2025-11-29 11:16:31 +0530

    Prepare for Render deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6bm1=--y7gh$9ko66%bibn2m63b+qq_tlo_(kibx!ceqy#9gb1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
